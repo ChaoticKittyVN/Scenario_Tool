3162034c39a7d3aa2ab190362ff63f6fadc1c122
Kirmy <kirmy@qq.com>
2025-11-14 18:31:08 +0800

    更新renpy版表格

--- param_config/param_mappings.py
+++ param_config/param_mappings.py
@@ -1,4 +1,4 @@
 # 自动生成的参数映射
 # 请不要手动编辑此文件
 
-PARAM_MAPPINGS = {'Music': {'停止': 'stop'}, 'Sound': {'停止': 'stop'}, 'Ambience': {'停止': 'stop'}, 'Background': {}, 'Event': {}, 'Id': {}, 'Speaker': {}, 'Character': {}, 'Pose': {'中': 'M', '左': 'L', '右': 'R'}, 'Animation': {}, 'Movie': {}, 'Transition': {}, '基础参数模板': {}}
+PARAM_MAPPINGS = {'Layer': {'主要': 'master', '遮盖': 'cover', '滤镜': 'cap', '近景': 'close'}, 'Transform': {'中': 'center', '左': 'left', '右': 'right'}, 'Transition': {'无': 'None # 无转场', '渐变': 'Dissolve', '圆角方进': 'SquareIn', '圆角方出': 'SquareOut', '睁眼': 'EyeOpen', '闭眼': 'EyeClose', '空': 'empty'}, 'Speaker': {}, 'Character': {}, 'Varient': {}, 'Animation': {}, 'Background': {'黑屏': 'black'}, 'Event': {}, 'Movie': {}, 'Music': {'停止': 'stop'}, 'Ambience': {}, 'Sound': {'停止': 'stop'}, 'Window': {}, '基础参数模板': {}}
